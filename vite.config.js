687dc9df3ecb8215c5cfcbbdf95051d08b6daa76
Sunit Mathur <sunit.mathur@gmail.com>
2026-02-28 14:52:13 -0500

    PWA: Forced service worker update and cache-busting

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        skipWaiting: true,
+        clientsClaim: true
+      },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Sara & Sunit Menu Planner',
